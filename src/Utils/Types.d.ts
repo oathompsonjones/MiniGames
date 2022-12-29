6103823c4adac538f3337dfb86d48370da2d521d
Ollie Jones <oathompsonjones@gmail.com>
2022-12-29 17:30:52 +0100

    Remove unneeded types

--- src/Utils/Types.d.ts
+++ src/Utils/Types.d.ts
@@ -23,24 +23,6 @@ type Position<
 ][Dimensions];
 
 // Ranges
-type CreateArrayWithLengthX<Length extends number, Acc extends unknown[] = []> = Acc["length"] extends Length
-    ? Acc
-    : CreateArrayWithLengthX<Length, [...Acc, 1]>;
-
-type NumericRange<StartArr extends number[], End extends number, Acc extends number = never> =
-    StartArr["length"] extends End
-        ? Acc
-        : NumericRange<[...StartArr, 1], End, Acc | StartArr["length"]>;
-
-type RangeFromStart<Start extends number, End extends number> = NumericRange<CreateArrayWithLengthX<Start>, End>;
-
+type CreateArrayWithLengthX<Length extends number, Acc extends unknown[] = []> = Acc["length"] extends Length ? Acc : CreateArrayWithLengthX<Length, [...Acc, 1]>;
+type NumericRange<StartArr extends number[], End extends number, Acc extends number = never> = StartArr["length"] extends End ? Acc : NumericRange<[...StartArr, 1], End, Acc | StartArr["length"]>;
 type Range<End extends number> = NumericRange<[], End>;
-
-type InclusiveNumericRange<StartArr extends number[], End extends number, Acc extends number = never> =
-    StartArr["length"] extends End
-        ? Acc | End
-        : NumericRange<[...StartArr, 1], End, Acc | StartArr["length"]>;
-
-type InclusiveRangeFromStart<Start extends number, End extends number> = InclusiveNumericRange<CreateArrayWithLengthX<Start>, End>;
-
-type InclusiveRange<End extends number> = InclusiveNumericRange<[], End>;
