0908f142fc6691ea1634fa6b665ea15d53f8cd0a
Ollie Jones <oathompsonjones@gmail.com>
2022-12-29 17:50:04 +0100

    Document the base classes.

--- src/Base/Board.ts
+++ src/Base/Board.ts
@@ -2,61 +2,148 @@ import { IntBitBoard, LongInt, LongIntBitBoard } from "../BitBoard";
 import type { MoveDimensions, Position, Range } from "../Utils";
 import type { BitBoard } from "../BitBoard";
 
-export abstract class Board<
-    Width extends number,
-    Height extends number,
-    BitBoardType extends BitBoard,
-    MoveType extends MoveDimensions
-> {
+/**
+ * Represent a game board.
+ *
+ * @abstract
+ * @class Board
+ * @typedef {Board}
+ * @template Width extends number
+ * @template Height extends number
+ * @template BitBoardType extends BitBoard
+ * @template MoveType extends MoveDimensions
+ */
+export abstract class Board<Width extends number, Height extends number, BitBoardType extends BitBoard, MoveType extends MoveDimensions> {
+    /**
+     * Contains the data stored in a BitBoard.
+     *
+     * @protected
+     * @readonly
+     * @type {BitBoardType}
+     */
     protected readonly data: BitBoardType;
 
+    /**
+     * The width of the board.
+     *
+     * @protected
+     * @readonly
+     * @type {Width}
+     */
     protected readonly boardWidth: Width;
 
+    /**
+     * The height of the board.
+     *
+     * @protected
+     * @readonly
+     * @type {Height}
+     */
     protected readonly boardHeight: Height;
 
-    private readonly bitsPerBoard: number;
-
+    /**
+     * How many boards there are representing player positions (most likely 2).
+     *
+     * @private
+     * @readonly
+     * @type {number}
+     */
     private readonly numberOfPlayerBoards: number;
 
+    /**
+     * Number of boards in total (most likely also 2).
+     *
+     * @private
+     * @readonly
+     * @type {number}
+     */
     private readonly numberOfBoards: number;
 
+    /**
+     * A stack of moves.
+     *
+     * @private
+     * @readonly
+     * @type {number[]}
+     */
     private readonly moves: number[] = [];
 
+    /**
+     * The board states which represent a winning state.
+     *
+     * @protected
+     * @abstract
+     * @readonly
+     * @type {BitBoardType[]}
+     */
     protected abstract readonly winningStates: BitBoardType[];
 
-    protected constructor(width: Width, height: Height, playerBoardCount: number, extraBoardCount: number = 0) {
+    /**
+     * Creates an instance of Board.
+     *
+     * @constructor
+     * @protected
+     * @param {Width} width The width of the board.
+     * @param {Height} height The height of the board.
+     * @param {number} playerBoardCount How many boards there are representing player positions (most likely 2).
+     * @param {number} [extraBoardCount=0] Number of extra boards (most likely 0).
+     */
+    protected constructor(width: Width, height: Height, playerBoardCount: number = 2, extraBoardCount: number = 0) {
         this.boardWidth = width;
         this.boardHeight = height;
-        this.bitsPerBoard = this.boardWidth * this.boardHeight;
         this.numberOfPlayerBoards = playerBoardCount;
         this.numberOfBoards = this.numberOfPlayerBoards + extraBoardCount;
-        const totalBits = this.bitsPerBoard * this.numberOfBoards;
+        const totalBits = this.boardWidth * this.boardHeight * this.numberOfBoards;
         this.data = (totalBits > 32 ? new LongIntBitBoard(Math.ceil(totalBits / 32)) : new IntBitBoard()) as BitBoardType;
     }
 
+    /**
+     * Calculates whether or not the board is full.
+     *
+     * @public
+     * @readonly
+     * @type {boolean}
+     */
     public get isFull(): boolean {
         let isFull = (this.data instanceof LongIntBitBoard
-            ? new LongIntBitBoard(Math.ceil(this.bitsPerBoard / 32))
+            ? new LongIntBitBoard(Math.ceil(this.boardWidth * this.boardHeight / 32))
             : new IntBitBoard()) as BitBoardType;
         for (let i = 0; i < this.numberOfPlayerBoards; i++)
             isFull = isFull.or(this.getPlayerBoard(i)) as BitBoardType;
         if (isFull instanceof LongIntBitBoard) {
-            const arr = Array<number>(Math.ceil(this.bitsPerBoard / 32)).fill(~0 >>> 0);
-            arr[arr.length - 1] = 2 ** (this.bitsPerBoard - (arr.length - 1) * 32) - 1;
+            const arr = Array<number>(Math.ceil(this.boardWidth * this.boardHeight / 32)).fill(~0 >>> 0);
+            arr[arr.length - 1] = 2 ** (this.boardWidth * this.boardHeight - (arr.length - 1) * 32) - 1;
             return isFull.equals(new LongInt(arr));
         }
-        return isFull.equals(2 ** this.bitsPerBoard - 1);
+        return isFull.equals(2 ** this.boardWidth * this.boardHeight - 1);
     }
 
+    /**
+     * Calculates whether or not the board is empty.
+     *
+     * @public
+     * @readonly
+     * @type {boolean}
+     */
     public get isEmpty(): boolean {
         let isEmpty = (this.data instanceof LongIntBitBoard
-            ? new LongIntBitBoard(Math.ceil(this.bitsPerBoard / 32))
+            ? new LongIntBitBoard(Math.ceil(this.boardWidth * this.boardHeight / 32))
             : new IntBitBoard()) as BitBoardType;
         for (let i = 0; i < this.numberOfPlayerBoards; i++)
             isEmpty = isEmpty.or(this.getPlayerBoard(i)) as BitBoardType;
         return isEmpty.equals(0);
     }
 
+    /**
+     * Calculates who the winner is.
+     * If the game is not over, the output is false.
+     * If there is a winner, the output is their ID.
+     * If there is a draw, the output is null.
+     *
+     * @public
+     * @readonly
+     * @type {(number | false | null)}
+     */
     public get winner(): number | false | null {
         const playerOneBoard = this.getPlayerBoard(0);
         const playerTwoBoard = this.getPlayerBoard(1);
@@ -71,6 +158,13 @@ export abstract class Board<
         return false;
     }
 
+    /**
+     * Calculates which cells are empty.
+     *
+     * @public
+     * @readonly
+     * @type {Array<Position<MoveType, Range<Width>, Range<Height>>>}
+     */
     public get emptyCells(): Array<Position<MoveType, Range<Width>, Range<Height>>> {
         const emptyCells: Array<Position<MoveType, Range<Width>, Range<Height>>> = [];
         for (let y = 0; y < this.boardHeight; y++) {
@@ -83,14 +177,34 @@ export abstract class Board<
         return emptyCells;
     }
 
+    /**
+     * Calculates the heuristic score for a given board state.
+     *
+     * @public
+     * @abstract
+     * @readonly
+     * @type {number}
+     */
     public abstract get heuristic(): number;
 
+    /**
+     * Makes a move on the board.
+     *
+     * @public
+     * @param {Position<MoveType, Range<Width>, Range<Height>>} move The position of the move.
+     * @param {number} playerId The player who's making the move.
+     */
     public makeMove(move: Position<MoveType, Range<Width>, Range<Height>>, playerId: number): void {
         const bit = this.getBitIndex(move, playerId);
         this.moves.push(bit);
         this.data.setBit(bit);
     }
 
+    /**
+     * Reverses the last move.
+     *
+     * @public
+     */
     public undoLastMove(): void {
         const lastMove = this.moves.pop();
         if (lastMove === undefined)
@@ -98,6 +212,13 @@ export abstract class Board<
         this.data.clearBit(lastMove);
     }
 
+    /**
+     * Checks which player is occupying a given cell.
+     *
+     * @public
+     * @param {Position<MoveType, Range<Width>, Range<Height>>} cell The cell to check.
+     * @returns {(number | null)} If the cell is empty, the output is null, otherwise the output is the player's ID.
+     */
     public cellOccupier(cell: Position<MoveType, Range<Width>, Range<Height>>): number | null {
         for (let i = 0; i < this.numberOfPlayerBoards; i++) {
             if (this.data.getBit(this.getBitIndex(cell, i)) === 1)
@@ -106,6 +227,12 @@ export abstract class Board<
         return null;
     }
 
+    /**
+     * Creates a string representation of the board.
+     *
+     * @public
+     * @returns {string} The string representation.
+     */
     public toString(): string {
         let unformattedBoard = "";
         for (let y = 0; y < this.boardHeight; y++) {
@@ -117,19 +244,50 @@ export abstract class Board<
         return unformattedBoard;
     }
 
+    /**
+     * Gets a bit index from its coordinates and player ID.
+     *
+     * @protected
+     * @param {Position<MoveType, Range<Width>, Range<Height>>} move The coordinates.
+     * @param {number} playerId The player ID.
+     * @returns {number} The bit index.
+     */
     protected getBitIndex(move: Position<MoveType, Range<Width>, Range<Height>>, playerId: number): number {
         const moveIndex = this.getIndex(move);
-        const bitBoardMoveIndex = moveIndex + this.bitsPerBoard * playerId;
+        const bitBoardMoveIndex = moveIndex + this.boardWidth * this.boardHeight * playerId;
         return bitBoardMoveIndex;
     }
 
+    /**
+     * Gets a bit index from its coordinates.
+     *
+     * @private
+     * @param {Position<MoveType, Range<Width>, Range<Height>>} move The coordinates.
+     * @returns {number} The bit index.
+     */
     private getIndex(move: Position<MoveType, Range<Width>, Range<Height>>): number {
         if (typeof move === "object" && move !== null && "x" in move && "y" in move)
             return (this.boardWidth as number) * (move.y as number) + (move.x as number);
         return move as number;
     }
 
+    /**
+     * Checks if a move is valid.
+     *
+     * @public
+     * @abstract
+     * @param {Position<MoveType>} move The move.
+     * @returns {boolean} Whether or not it's valid.
+     */
     public abstract moveIsValid(move: Position<MoveType>): boolean;
 
+    /**
+     * A BitBoard containing only the player's bits.
+     *
+     * @protected
+     * @abstract
+     * @param {number} playerId The player's ID.
+     * @returns {BitBoardType} The player's bits.
+     */
     protected abstract getPlayerBoard(playerId: number): BitBoardType;
 }
--- src/Base/Controller.ts
+++ src/Base/Controller.ts
@@ -3,34 +3,93 @@ import type { BitBoard } from "../BitBoard";
 import type { Board } from "./";
 import { RenderType } from "../Utils/Constants";
 
+/**
+ * Represents a game controller.
+ *
+ * @abstract
+ * @class Controller
+ * @typedef {Controller}
+ * @template BoardWidth extends number
+ * @template BoardHeight extends number
+ * @template BoardType extends BitBoard
+ * @template MoveType extends MoveDimensions = MoveDimensions.TwoDimensional
+ */
 export abstract class Controller<
     BoardWidth extends number,
     BoardHeight extends number,
     BoardType extends BitBoard,
     MoveType extends MoveDimensions = MoveDimensions.TwoDimensional
 > {
+    /**
+     * Contains the board.
+     *
+     * @protected
+     * @readonly
+     * @type {Board<BoardWidth, BoardHeight, BoardType, MoveType>}
+     */
     protected readonly board: Board<BoardWidth, BoardHeight, BoardType, MoveType>;
 
+    /**
+     * Contains the player objects.
+     *
+     * @protected
+     * @readonly
+     * @type {Array<Player<PlayerType>>}
+     */
     protected readonly players: Array<Player<PlayerType>>;
 
+    /**
+     * Contains the rendering type.
+     *
+     * @private
+     * @readonly
+     * @type {RenderType}
+     */
     private readonly renderType: RenderType;
 
+    /**
+     * Contains the ID of the current player.
+     *
+     * @private
+     * @type {number}
+     */
     private currentPlayerId: number = 0;
 
-    protected constructor(
-        board: Board<BoardWidth, BoardHeight, BoardType, MoveType>,
-        playerTypes: PlayerType[],
-        renderType: RenderType
-    ) {
+    /**
+     * Creates an instance of Controller.
+     *
+     * @constructor
+     * @protected
+     * @param {Board<BoardWidth, BoardHeight, BoardType, MoveType>} board The board.
+     * @param {PlayerType[]} playerTypes The types of player.
+     * @param {RenderType} renderType The rendering type.
+     */
+    protected constructor(board: Board<BoardWidth, BoardHeight, BoardType, MoveType>, playerTypes: PlayerType[], renderType: RenderType) {
         this.board = board;
         this.players = playerTypes.map((playerType, id) => ({ id, playerType }));
         this.renderType = renderType;
     }
 
+    /**
+     * Gets the current player object.
+     *
+     * @public
+     * @readonly
+     * @type {Player<PlayerType>}
+     */
     public get currentPlayer(): Player<PlayerType> {
         return this.players[this.currentPlayerId]!;
     }
 
+    /**
+     * The minimax algorithm.
+     *
+     * @link https://en.wikipedia.org/wiki/Minimax
+     * @public
+     * @param {number} [depth=Infinity] The depth of the algorithm.
+     * @param {boolean} [maximisingPlayer=true] Whether or not the current player is the maximising player.
+     * @returns {Minimax<MoveType, BoardWidth, BoardHeight>} The optimal move.
+     */
     public minimax(depth: number = Infinity, maximisingPlayer: boolean = true): Minimax<MoveType, BoardWidth, BoardHeight> {
         const playerIds = [(this.currentPlayerId + 1) % 2, this.currentPlayerId];
         if (depth === 0 || this.board.winner !== false) {
@@ -50,10 +109,21 @@ export abstract class Controller<
         return scores.sort((a, b) => (maximisingPlayer ? a.score - b.score : b.score - a.score))[0]!;
     }
 
+    /**
+     * Changes which player's turn it is.
+     *
+     * @public
+     */
     public nextTurn(): void {
         this.currentPlayerId = (this.currentPlayerId + 1) % this.players.length;
     }
 
+    /**
+     * Renders the game.
+     *
+     * @public
+     * @param {(number | false | null)} winner The output of get winner.
+     */
     public render(winner: number | false | null): void {
         switch (this.renderType) {
             case RenderType.Canvas:
@@ -63,6 +133,13 @@ export abstract class Controller<
         }
     }
 
+    /**
+     * Gets the game input.
+     *
+     * @public
+     * @async
+     * @returns {Promise<Position<MoveType>>} The input.
+     */
     public async getInput(): Promise<Position<MoveType>> {
         switch (this.renderType) {
             case RenderType.Canvas:
@@ -72,6 +149,13 @@ export abstract class Controller<
         }
     }
 
+    /**
+     * Gets a valid move input.
+     *
+     * @public
+     * @async
+     * @returns {Promise<Position<MoveType, Range<BoardWidth>, Range<BoardHeight>>>} The valid move.
+     */
     public async getValidMove(): Promise<Position<MoveType, Range<BoardWidth>, Range<BoardHeight>>> {
         let move: Position<MoveType>;
         do
@@ -81,11 +165,67 @@ export abstract class Controller<
         return move as Position<MoveType, Range<BoardWidth>, Range<BoardHeight>>;
     }
 
+    /**
+     * Controls the main game flow.
+     *
+     * @public
+     * @abstract
+     * @returns {(Promise<number | null>)} The winner.
+     */
     public abstract play(): Promise<number | null>;
+
+    /**
+     * Gets input form the canvas.
+     *
+     * @public
+     * @abstract
+     * @returns {Promise<Position<MoveType>>} The input.
+     */
     public abstract getCanvasInput(): Promise<Position<MoveType>>;
+
+    /**
+     * Gets input from the console.
+     *
+     * @public
+     * @abstract
+     * @returns {Promise<Position<MoveType>>} The input.
+     */
     public abstract getConsoleInput(): Promise<Position<MoveType>>;
+
+    /**
+     * Renders to the canvas.
+     *
+     * @public
+     * @abstract
+     * @param {(number | false | null)} winner The output of get winner.
+     */
     public abstract renderToCanvas(winner: number | false | null): void;
+
+    /**
+     * Renders to the console.
+     *
+     * @public
+     * @abstract
+     * @param {(number | false | null)} winner The output of get winner.
+     */
     public abstract renderToConsole(winner: number | false | null): void;
+
+    /**
+     * Determines the CPU move.
+     *
+     * @public
+     * @abstract
+     * @param {Omit<PlayerType, PlayerType.Human>} difficulty The difficulty of the AI.
+     * @returns {Position<MoveType, Range<BoardWidth>, Range<BoardHeight>>} The move.
+     */
     public abstract determineCPUMove(difficulty: Omit<PlayerType, PlayerType.Human>): Position<MoveType, Range<BoardWidth>, Range<BoardHeight>>;
+
+    /**
+     * Finds the optimal move.
+     *
+     * @public
+     * @abstract
+     * @returns {(Position<MoveType, Range<BoardWidth>, Range<BoardHeight>> | null)} The optimal move.
+     */
     public abstract findOptimalMove(): Position<MoveType, Range<BoardWidth>, Range<BoardHeight>> | null;
 }
