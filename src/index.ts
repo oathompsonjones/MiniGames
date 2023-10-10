aa2731cc2bbdf869cf13c8f50dd61f830e9c810d
Ollie Jones <oathompsonjones@gmail.com>
2023-10-10 10:36:41 +0100

    Allow the process to loop until the player chooses to stop

--- src/index.ts
+++ src/index.ts
@@ -8,60 +8,68 @@ const games = [
     Connect4
 ];
 
-Console.clear();
-
-const start = await Console.readLine("Would you like to play a game? (Y/N) ");
-if (start.toLowerCase() !== "y")
-    process.exit(0);
-
-let gameChoiceIndex = NaN;
-
-do {
+async function input(message: string): Promise<string> {
     Console.clear();
-    // eslint-disable-next-line no-await-in-loop
-    const selectGame = await Console.readLine(`Select a game (1-${games.length}):\n${games
-        .map(({ name }, i) => `${i + 1}. ${name}`)
-        .join("\n")}\n`);
-    gameChoiceIndex = parseInt(selectGame, 10) - 1;
-} while (isNaN(gameChoiceIndex) || gameChoiceIndex < 0 || gameChoiceIndex > games.length - 1);
+    return Console.readLine(message);
+}
 
-let playerCount = NaN;
+await (async function main(): Promise<void> {
+    const start = await input("Would you like to play a game? (Y/N) ");
+    if (start.toLowerCase() !== "y")
+        process.exit(0);
 
-do {
-    Console.clear();
-    // eslint-disable-next-line no-await-in-loop
-    const selectPlayers = await Console.readLine("How many players? (0-2) ");
-    playerCount = parseInt(selectPlayers, 10);
-} while (isNaN(playerCount) || playerCount < 0 || playerCount > 2);
+    let gameChoiceIndex = NaN;
 
-let difficulty: PlayerType | undefined;
-
-if (playerCount === 1) {
     do {
-        Console.clear();
         // eslint-disable-next-line no-await-in-loop
-        const selectDifficulty = await Console.readLine([
-            "Select a difficulty (1-4):",
-            "1. Easy",
-            "2. Medium",
-            "3. Hard",
-            "4. Impossible\n"
-        ].join("\n"));
-        const difficulties: PlayerType[] = ["easyCPU", "mediumCPU", "hardCPU", "impossibleCPU"];
-        difficulty = difficulties[parseInt(selectDifficulty, 10) - 1];
-    } while (difficulty === undefined);
-}
+        const selectGame = await input(`Select a game (1-${games.length}):\n${games
+            .map(({ name }, i) => `${i + 1}. ${name}`)
+            .join("\n")}\n`);
+        gameChoiceIndex = parseInt(selectGame, 10) - 1;
+    } while (isNaN(gameChoiceIndex) || gameChoiceIndex < 0 || gameChoiceIndex > games.length - 1);
 
-const Game = games[gameChoiceIndex]!;
-const playerOneType = playerCount > 0 ? "human" : "impossibleCPU";
-const playerTwoType = playerCount > 1 ? "human" : difficulty ?? "impossibleCPU";
+    let playerCount = NaN;
 
-const winners = [];
-if (playerCount > 0) {
-    void new Game("console", playerOneType, playerTwoType).play();
-} else {
-    for (let i = 0; i < 1000; i++)
-        winners.push(new Game("console", playerOneType, playerTwoType).play());
-    const alwaysATie = (await Promise.all(winners)).every((winner) => winner === null);
-    Console.writeLine(alwaysATie ? `${winners.length} ties!` : "Not always a draw");
-}
+    do {
+        // eslint-disable-next-line no-await-in-loop
+        const selectPlayers = await input("How many players? (0-2) ");
+        playerCount = parseInt(selectPlayers, 10);
+    } while (isNaN(playerCount) || playerCount < 0 || playerCount > 2);
+
+    let difficulty: PlayerType | undefined;
+
+    if (playerCount === 1) {
+        do {
+            // eslint-disable-next-line no-await-in-loop
+            const selectDifficulty = await input([
+                "Select a difficulty (1-4):",
+                "1. Easy",
+                "2. Medium",
+                "3. Hard",
+                "4. Impossible\n"
+            ].join("\n"));
+            difficulty = (["easyCPU", "mediumCPU", "hardCPU", "impossibleCPU"] as const)[parseInt(selectDifficulty, 10) - 1];
+        } while (difficulty === undefined);
+    }
+
+    const Game = games[gameChoiceIndex]!;
+    const playerOneType = playerCount > 0 ? "human" : "impossibleCPU";
+    const playerTwoType = playerCount > 1 ? "human" : difficulty ?? "impossibleCPU";
+
+    const winners = [];
+    if (playerCount > 0) {
+        await new Game("console", playerOneType, playerTwoType).play();
+    } else {
+        for (let i = 0; i < 1000; i++)
+            winners.push(new Game("console", playerOneType, playerTwoType).play());
+        Console.writeLine(
+            (await Promise.all(winners)).every((winner) => winner === null)
+                ? `${winners.length} ties!`
+                : "Not always a draw"
+        );
+    }
+
+    await Console.readLine("");
+
+    return main();
+})();
