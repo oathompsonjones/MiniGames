f1abce85988942b3aa274f45d337751e0504ae86
Ollie Jones <oathompsonjones@gmail.com>
2023-12-21 02:03:54 +0000

    Export type Game

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as TicTacToe } from "./games/tictactoe/controller.js";
 export { default as Connect4 } from "./games/connect4/controller.js";
+export type { default as Game } from "./base/controller.js";
